908f23f82354bfbbb774b4df745a92aa79f0b403
Lana <holyalex997@gmail.com>
2025-09-16 10:20:56 +0300

    add a fitness tracker module

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,27 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, data := range dataset {
+		err := dp.Parse(data)
+		if err != nil {
+			log.Printf("parse error: %v\n", err)
+			continue
+		}
+		info, err := dp.ActionInfo()
+		if err != nil {
+			log.Printf("action info error: %v\n", err)
+			continue
+		}
+		fmt.Println(info)
+	}
 }
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -12,18 +13,38 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
 )
 
+// Функция принимает количество шагов, вес и рост, продолжительность бега и 
+// возвращает количество калорий с корректирующим коэффициентом, error
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("Empty variable value")
+	} else {
+		return ((weight * MeanSpeed(steps, height, duration) * duration.Minutes()) / minInH) * walkingCaloriesCoefficient, nil
+	}
 }
 
+// Функция принимает количество шагов, вес и рост, продолжительность бега и 
+// возвращает количество калорий, error
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("Empty variable value")
+	} else {
+		return (weight * MeanSpeed(steps, height, duration) * duration.Minutes()) / minInH, nil
+	}
 }
 
+// Функция принимает количество шагов, рост пользователя и продолжительность 
+// активности и возвращает среднюю скорость
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	} else { 
+		return Distance(steps, height) / duration.Hours()
+	}
 }
 
+// Функция принимает количество шагов и рост пользователя в метрах и 
+// возвращает дистанцию в километрах
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	return float64(steps) * (height * stepLengthCoefficient) / mInKm
 }
